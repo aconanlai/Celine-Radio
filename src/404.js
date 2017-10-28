dc9a372b30adec8be35fd3655c25e7967c6bbef7
jamal jean - tobias <>
2017-10-28 19:25:09 -0400

    Scafolding reactrouter4

---
+++ src/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// THis is a test - will not be implemnted 
+
+
+const Error404 = () => {
+  return(
+    <h1>OOPS looks like we have a 404</h1>
+  )
+
+}
+
+export default Error404;
